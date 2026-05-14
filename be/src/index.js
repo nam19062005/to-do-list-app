f2d7ba31583fec370fb6596a81f5fa0d7e5fba3d
BASNAM <nam65622343@gmail.com>
2026-05-14 10:12:03 +0700

    lan 2

--- be/src/index.js
+++ be/src/index.js
@@ -3,18 +3,27 @@ import taskRoutes from "./routes/taskRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
-
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 const app = express();
 
 // Middleware để parse JSON
 app.use(express.json());
-app.use(cors());
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors());
+}
 
 app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../fe/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../fe/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
